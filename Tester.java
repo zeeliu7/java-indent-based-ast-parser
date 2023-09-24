39070dfc152289965e42669132e801bfa6094e64
Zhonghao Liu <141634269+zeeliu7@users.noreply.github.com>
2023-09-24 11:18:48 -0500

    Update Tester.java
    changed file path description

--- Tester.java
+++ Tester.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 
 public class Tester {
     public static void main (String[] args) throws FileNotFoundException, IOException {
-        String filePath = "/Users/tonyliu/Desktop/BrokenCodeSample.java";
+        String filePath = "your_input_file_path.java";
         FileTerminal ft = new FileTerminal(filePath);
-        ft.fixFile("/Users/tonyliu/Desktop/BrokenCodeFixed.java");
+        ft.fixFile("your_output_file_path.java");
     }
 }
